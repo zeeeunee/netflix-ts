081a05dea7e19aa3e86a5ac7c779d00fd7b1b568
zeeeunee <wldmstv@gmail.com>
2024-01-26 18:20:21 +0900

    기본 스타일 지정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 const Header = () => {
 	return (
-		<header>
-			<h1>NETFLIX</h1>
-			<ul>
-				<li className='headerLink'>menu</li>
-				<li className='headerLink'>menu</li>
-				<li className='headerLink'>menu</li>
-			</ul>
+		<header className='w-[100%]'>
+			<div className='w-[100%] flex justify-between items-center space-x-2 p-5'>
+				<h1>NETFLIX</h1>
+				<ul className='flex gap-8'>
+					<li className='headerLink'>menu</li>
+					<li className='headerLink'>menu</li>
+					<li className='headerLink'>menu</li>
+				</ul>
+			</div>
 		</header>
 	);
 };
