edd25bffc35604f7520f62d63302e927d996e439
zeeeunee <wldmstv@gmail.com>
2024-01-29 13:08:09 +0900

    tailwind config로 미리 atimation구문을 animate preset에 등록후 Image컴포넌트에 로딩바 보이도록 처리

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { Movie } from '../../types';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { baseURL } from '../../url';
 import { NextPage } from 'next';
 
@@ -9,6 +9,7 @@ interface Props {
 }
 
 const Banner: NextPage<Props> = ({ original }: Props) => {
+	const loading = useRef<any>(null);
 	const [Movie, setMovie] = useState<Movie | null>(null);
 
 	useEffect(() => {
@@ -32,10 +33,17 @@ const Banner: NextPage<Props> = ({ original }: Props) => {
 							quality={70}
 							sizes='(max-width:768px) 100vw, (max-width:1200) 70vw, 100vw'
 							className='object-cover'
+							onLoadingComplete={() => loading.current.remove()}
 						/>
 					</div>
 					{/* gradient layer */}
 					<div className='absolute z-[2] top-0 left-0 w-full h-full bg-gradient1'></div>
+
+					{/* loading bar */}
+					<div
+						ref={loading}
+						className='w-[40px] h-[40px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] border-4 border-[orange] border-t-[transparent] rounded-[50%] z-[3] animate-ani-rotation'></div>
+
 					{/* title */}
 					<h1 className='relative z-[3] text-2xl font-bold md:text-6xl'>{Movie?.title || Movie?.name}</h1>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,15 @@ const config: Config = {
 			},
 			backgroundImage: {
 				gradient1: 'linear-gradient(to bottom, rgba(20,20,20,0), rgba(20,20,20,1))'
+			},
+			keyframes: {
+				rotation: {
+					'0%': { transform: 'rotate(0deg)' },
+					'100%': { transform: 'rotate(360deg)' }
+				}
+			},
+			animation: {
+				'ani-rotation': 'rotation 1s linear infinite'
 			}
 		}
 	},
