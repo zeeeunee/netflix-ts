cf5b1ce4cec665ef99623faf5df8672c622e9083
zeeeunee <wldmstv@gmail.com>
2024-01-30 13:22:56 +0900

    전역 스테이트에 전달할 회원가입함수 추가

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -29,5 +29,43 @@ const AuthContext = createContext<IAuth>({
 });
 //전체 컴포넌트를 감싸줄 Wrapping컴포넌트 (전역데이터를 모든 컴포넌트에 전달하는 최상위 부모 컴포넌트)
 export const AuthProvider = ({ children }: AuthProviderProps) => {
+	const [Loading, setLoading] = useState<boolean>(false);
+	const [UserInfo, setUserInfo] = useState<User | null>(null);
+	const [Errors, setErrors] = useState<string>('');
+	const [InitialLoading, setInitialLoading] = useState<boolean>(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		//firebase로부터 전달되는 auth상태값이 변경될 때마다 해당 useEffect실행
+		onAuthStateChanged(auth, user => {
+			//인증상태가 변경 될 때마다 해당상태를 감지해서 전달 받은 인증정보가 있으면
+			if (user) {
+				setUserInfo(user);
+				setLoading(false);
+				router.push('/');
+			} else {
+				//전달받은 인증정보가 없으면
+				setUserInfo(user);
+				setLoading(true);
+				router.push('/login');
+			}
+			//한번이라도 인증로직이 실행되면 초기상태를 false로 변경
+			setInitialLoading(false);
+		});
+	}, [router]);
+
+	//회원가입함수
+	const signUp = async (email: string, password: string) => {
+		setLoading(true);
+		await createUserWithEmailAndPassword(auth, email, password)
+			.then(userInfo => {
+				setUserInfo(userInfo.user);
+				router.push('/');
+				setInitialLoading(false);
+			})
+			.catch(err => alert(err.message))
+			.finally(() => setLoading(false));
+	};
+
 	return <AuthContext.Provider value={{ UserInfo, signIn, signOut, signUp, Loading, Errors }}>{children}</AuthContext.Provider>;
 };
