7091a96adcfe5f4593bfba7cb69e0b788a5df21d
zeeeunee <wldmstv@gmail.com>
2024-01-30 10:53:55 +0900

    로그인배경이미지에 로딩바연결

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { FunctionComponent } from 'react';
+import { BounceLoader } from 'react-spinners';
+import { useState } from 'react';
 
 const Login: FunctionComponent = () => {
+	const [IsLoading, setIsLoading] = useState(true);
 	return (
 		<main>
 			<Head>
@@ -10,9 +13,24 @@ const Login: FunctionComponent = () => {
 				<link rel='icon' href='/public/favicon.ico' />
 			</Head>
 
-			<Image src='https://rb.gy/p2hphi' fill priority alt='login Page' className='w-full h-screen z-[10] opacity-50 object-cover' />
+			<div className='relative w-full h-screen overflow-hidden'>
+				<Image
+					src='https://rb.gy/p2hphi'
+					fill
+					priority
+					alt='login Page'
+					className='w-full h-screen z-[1] opacity-50 object-cover'
+					onLoadingComplete={() => setIsLoading(false)}
+				/>
+
+				<BounceLoader
+					size={100}
+					loading={IsLoading}
+					color='orange'
+					className='absolute top-[50%] left-[50%] ml-[-50px] mt-[-50px] z-[2] opacity-100'
+				/>
+			</div>
 		</main>
 	);
 };
-
 export default Login;
