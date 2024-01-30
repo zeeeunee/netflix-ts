4f02b92befcd4b2359d81282915ac02256cd6b90
zeeeunee <wldmstv@gmail.com>
2024-01-30 14:35:01 +0900

    전역 State에 옮겨담는 데이터 구조 변경

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,71 +1,69 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, User } from 'firebase/auth';
 import { useRouter } from 'next/router';
-import React, { createContext, useContext, useEffect, useState, useMemo } from 'react';
+import React, { createContext, useContext, useRef, useEffect, useState, useMemo } from 'react';
 import { auth } from '@/firebase';
+
+interface Iloading {
+	current: boolean;
+}
 
 //전역 Context에 전달할 인증정보 타입
 interface IAuth {
 	UserInfo: User | null;
 	signIn: (email: string, password: string) => Promise<void>;
 	signUp: (email: string, password: string) => Promise<void>;
-	signOut: () => Promise<void>;
-	Errors: string | null;
-	Loading: boolean;
+	logout: () => Promise<void>;
+	InitialLoading: Iloading;
 }
 
-//전역 Stateprovider에 전달할 Props타입
+//전역State provider에 전달할 Props 타입
 interface AuthProviderProps {
 	children: React.ReactNode;
 }
-
-//firebase로부터 전달받아서 전역 Context에 전달할 객체모음
+//firebase로부터 전달받아서 전역Context에 전달할 객체모음
 const AuthContext = createContext<IAuth>({
 	UserInfo: null,
 	signUp: async () => {},
 	signIn: async () => {},
-	signOut: async () => {},
-	Errors: null,
-	Loading: false
+	logout: async () => {},
+	InitialLoading: { current: true }
 });
+
 //전체 컴포넌트를 감싸줄 Wrapping컴포넌트 (전역데이터를 모든 컴포넌트에 전달하는 최상위 부모 컴포넌트)
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-	const [Loading, setLoading] = useState<boolean>(false);
 	const [UserInfo, setUserInfo] = useState<User | null>(null);
-	const [Errors, setErrors] = useState<string>('');
-	const [InitialLoading, setInitialLoading] = useState<boolean>(true);
+	//초기에 한번 로딩완료되면 더이상 바뀔일이 없는 값인데 굳이 state에 담아서 불필요한 재렌더링을 방지하기 위해 useRef에 담아줌
+	const InitialLoading = useRef<boolean>(true);
 	const router = useRouter();
 
 	useEffect(() => {
-		//firebase로부터 전달되는 auth상태값이 변경될 때마다 해당 useEffect실행
+		//firebase로부터 전달되는 Auth상태값이 변경될때마다 해당 useEffect실행
 		onAuthStateChanged(auth, user => {
-			//인증상태가 변경 될 때마다 해당상태를 감지해서 전달 받은 인증정보가 있으면
+			//인증 상태가 변경될떄 해당 상태를 감지해서
+			//전달받은 인증정보가 있으면
 			if (user) {
 				setUserInfo(user);
-				setLoading(false);
 				router.push('/');
 			} else {
 				//전달받은 인증정보가 없으면
-				setUserInfo(user);
-				setLoading(true);
+				setUserInfo(null);
 				router.push('/login');
 			}
 			//한번이라도 인증로직이 실행되면 초기상태를 false로 변경
-			setInitialLoading(false);
+			setTimeout(() => (InitialLoading.current = false), 0);
 		});
 	}, [router]);
 
 	//회원가입함수
 	const signUp = async (email: string, password: string) => {
-		setLoading(true);
 		await createUserWithEmailAndPassword(auth, email, password)
 			.then(userInfo => {
 				setUserInfo(userInfo.user);
 				router.push('/');
-				setInitialLoading(false);
 			})
-			.catch(err => alert(err.message))
-			.finally(() => setLoading(false));
+			.catch(err => alert(err.message));
 	};
+
 	//로그인함수
 	const signIn = async (email: string, password: string) => {
 		await signInWithEmailAndPassword(auth, email, password)
@@ -75,7 +73,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 			})
 			.catch(err => alert(err.message));
 	};
-
 	//로그아웃 함수
 	const logout = async () => {
 		signOut(auth)
@@ -84,5 +81,15 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 			})
 			.catch(err => alert(err.message));
 	};
-	return <AuthContext.Provider value={{ UserInfo, signIn, signOut, signUp, Loading, Errors }}>{children}</AuthContext.Provider>;
+
+	//새로고침시 같은 로그인 정보값이면 해당 값을 다시 연산하지 않도록 메모이제이션처리해서 전역 context에 넘기고
+	const memoedContext = useMemo(() => ({ UserInfo, signIn, signUp, logout, InitialLoading }), [UserInfo]);
+
+	//로그인정보값이 들어와있을때에만 화면 출력
+	//실시간으로 적용되는 IntialLoading값을 전역 context에 담음
+	return <AuthContext.Provider value={memoedContext}>{children}</AuthContext.Provider>;
 };
+
+export default function useAuth() {
+	return useContext(AuthContext);
+}
