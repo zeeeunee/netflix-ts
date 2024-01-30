ad73fa629882d885b1e0c881b44c4ef6367f8a66
zeeeunee <wldmstv@gmail.com>
2024-01-30 12:20:10 +0900

    인증처리할 input항목 등록및 인증 실패시 출력될 에러메세지 설정

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -18,13 +18,19 @@ const Login: FunctionComponent = () => {
 	const [Login, setLogin] = useState<boolean>(false);
 
 	//register: 원하는 input요소를 전개연산자로 등록해서 값을 관리
-	//handleSubmit: submit이벤트 발생시 register에 등록된 input값들의 인증처리 함수
+	//handleSubmit: submit이벤트 발생시 register에 등록된 input값들의 인증처리 핸들러함수를 콜백으로 전달받음
 	//formState: 인증실패시 커스텀에러메세지를 등록할 수 있는 객체
 	const {
 		register,
 		handleSubmit,
 		formState: { errors } //formState객체값에서 다시 errors에 등록되어 있는 에러메세지만 추출
 	} = useForm<Inputs>();
+
+	const join: SubmitHandler<Inputs> = async ({ email, password }) => {
+		console.log('email', email);
+		console.log('password', password);
+	};
+
 	return (
 		<main>
 			<Head>
@@ -55,13 +61,15 @@ const Login: FunctionComponent = () => {
 				{/* logo */}
 				<Image width={150} height={150} src={logo} alt='nextflix' className='absolute left-4 top-4 cursor-pointer md:left-10 md:top-6 z-[3]' />
 
-				<form className='relative z-[5] bg-black/70 py-10 px-6 space-y-8'>
+				{/* submit이벤트 발생시 hadleSubmit이 인증처리를 해주고 인증의 결과값을 등록된 콜백함수에 전달 */}
+				<form onSubmit={handleSubmit(join)} className='relative z-[5] bg-black/70 py-10 px-6 space-y-8'>
 					<h1 className='text-4xl font-semibold'>Sign In</h1>
 
-					<div className='space-y-4'>
-						<input type='email' placeholder='Email' className='input' />
-						<input type='password' placeholder='Password' className='input' />
-					</div>
+					<input type='email' placeholder='Email' className='input' {...register('email', { required: true })} />
+					{/* 인증 실패시 비구조할당으로 뽑아낸 errors객체에 전달한 property명으로 에러값 전달 */}
+					{errors.email && <span>Enter a valid Email</span>}
+					<input type='password' placeholder='Password' className='input' {...register('password', { required: true })} />
+					{errors.password && <span>Enter a valid Password</span>}
 
 					<button className='w-full rounded bg-[red] py-3 font-semibold'>Sign In</button>
 
