5b8f92c577914fd2981496bd52c0127192d3b2d9
zeeeunee <wldmstv@gmail.com>
2024-01-30 13:05:42 +0900

    커스텀훅 안에서 로그인 정보객체를 받아서 전역 context에 전달해주는 Provider컴포넌트 생성

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import React, { createContext, useContext, useEffect, useState, useMemo } from 'react';
 import { auth } from '@/firebase';
 
+//전역 Context에 전달할 인증정보 타입
 interface IAuth {
 	UserInfo: User | null;
 	signIn: (email: string, password: string) => Promise<void>;
@@ -12,10 +13,12 @@ interface IAuth {
 	Loading: boolean;
 }
 
+//전역 Stateprovider에 전달할 Props타입
 interface AuthProviderProps {
 	children: React.ReactNode;
 }
 
+//firebase로부터 전달받아서 전역 Context에 전달할 객체모음
 const AuthContext = createContext<IAuth>({
 	UserInfo: null,
 	signUp: async () => {},
@@ -24,7 +27,7 @@ const AuthContext = createContext<IAuth>({
 	Errors: null,
 	Loading: false
 });
-
+//전체 컴포넌트를 감싸줄 Wrapping컴포넌트 (전역데이터를 모든 컴포넌트에 전달하는 최상위 부모 컴포넌트)
 export const AuthProvider = ({ children }: AuthProviderProps) => {
 	return <AuthContext.Provider value={{ UserInfo, signIn, signOut, signUp, Loading, Errors }}>{children}</AuthContext.Provider>;
 };
